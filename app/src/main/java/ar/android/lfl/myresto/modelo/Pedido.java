37f2487cabc402d99f6feaf52292cda8c396c5f4
LfLanzoni <lflanzoni@gmail.com>
2018-06-15 10:42:23 -0300

    clase pedido

---
+++ app/src/main/java/ar/android/lfl/myresto/modelo/Pedido.java
@@ -0,0 +1,106 @@
+package ar.android.lfl.myresto.modelo;
+
+public class Pedido {
+    private Integer id;
+    private String nombre;
+    private String pedido;
+    private Boolean envioDomicilio;
+    private Boolean bebidaXL;
+    private Boolean permiteCancelar;
+    private Boolean incluyePropina;
+    private Boolean enviarNotificaciones;
+    private Boolean pagoAutomatico;
+    private static int idGenerator = 0;
+
+
+    public Pedido(){
+        this.id = ++Pedido.idGenerator;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", pedido='" + pedido + '\'' +
+                ", envioDomicilio=" + envioDomicilio +
+                ", bebidaXL=" + bebidaXL +
+                ", permiteCancelar=" + permiteCancelar +
+                ", incluyePropina=" + incluyePropina +
+                ", enviarNotificaciones=" + enviarNotificaciones +
+                ", pagoAutomatico=" + pagoAutomatico +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(String pedido) {
+        this.pedido = pedido;
+    }
+
+    public Boolean getEnvioDomicilio() {
+        return envioDomicilio;
+    }
+
+    public void setEnvioDomicilio(Boolean envioDomicilio) {
+        this.envioDomicilio = envioDomicilio;
+    }
+
+    public Boolean getBebidaXL() {
+        return bebidaXL;
+    }
+
+    public void setBebidaXL(Boolean bebidaXL) {
+        this.bebidaXL = bebidaXL;
+    }
+
+    public Boolean getPermiteCancelar() {
+        return permiteCancelar;
+    }
+
+    public void setPermiteCancelar(Boolean permiteCancelar) {
+        this.permiteCancelar = permiteCancelar;
+    }
+
+    public Boolean getIncluyePropina() {
+        return incluyePropina;
+    }
+
+    public void setIncluyePropina(Boolean incluyePropina) {
+        this.incluyePropina = incluyePropina;
+    }
+
+    public Boolean getEnviarNotificaciones() {
+        return enviarNotificaciones;
+    }
+
+    public void setEnviarNotificaciones(Boolean enviarNotificaciones) {
+        this.enviarNotificaciones = enviarNotificaciones;
+    }
+
+    public Boolean getPagoAutomatico() {
+        return pagoAutomatico;
+    }
+
+    public void setPagoAutomatico(Boolean pagoAutomatico) {
+        this.pagoAutomatico = pagoAutomatico;
+    }
+}
